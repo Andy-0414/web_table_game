df0f69083835edfe214878d5bb6ed41e270b0c4b
Andy0414 <pjh8667@naver.com>
2019-02-26 16:30:56 +0900

    DELETE EJS

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('view engine', 'ejs');
-app.set('views', './views'); // EJS 사용
-
 app.use(express.static('docs'))
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.sendFile(__dirname+'/index.html')
 })
 
 io.on('connection', (socket)=>{
     console.log('hello')
 });
 
-http.listen(80, () => {
+http.listen(3000, () => {
     console.log("server open");
 })
